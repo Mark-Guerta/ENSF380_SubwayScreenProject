3875eab8ac85ae41a3849efe88b15427f5658222
Saif Youssef <seifeldiny1@gmail.com>
2024-07-19 20:01:42 -0600

    initialized matcher and pattern, succefully downloads the html page

--- weatherReport/src/module-info.java
+++ weatherReport/src/module-info.java
@@ -5,4 +5,5 @@
  * 
  */
 module weatherReport {
+	requires jsoup;
 }
--- weatherReport/src/seifeldinyoussef/ucalgary/ca/weatherReportParse.java
+++ weatherReport/src/seifeldinyoussef/ucalgary/ca/weatherReportParse.java
@@ -1,10 +1,42 @@
 package seifeldinyoussef.ucalgary.ca;
+import java.util.regex.*;
+import java.io.IOException;
+import java.util.Scanner;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 
 public class weatherReportParse {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//String html = Jsoup.connect("https://openweathermap.org/").get.html;
+		try {
+			Document document = Jsoup.connect("https://openweathermap.org/").get();
+			//System.out.println(document);
+			Scanner scanner = new Scanner(System.in);
+			System.out.println("Enter City ID:");
+			String cityID = scanner.nextLine();
+		
+			//System.out.println(cityID);
+			Pattern cityIDMatcher = Pattern.compile("city ID" + Pattern.quote(cityID));
+			Matcher matcher = cityIDMatcher.matcher(document.html());
+		
+			if(matcher.find()) {
+				System.out.println(matcher.group());	
+			} else {
+			System.out.println("City ID not found");
+			}
+		
 
+		
+			scanner.close();
+		}catch (IOException e) {
+			System.out.println(" City ID not found");
+			e.printStackTrace();
+			
+		}
 	}
-
-}
+}	
+	
+	 
+	
