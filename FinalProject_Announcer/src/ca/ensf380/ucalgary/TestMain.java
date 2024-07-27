16eb2e7af2062d3dd2f2bec02d143fbd360a6bdf
Arion <144377385+Mark-Guerta@users.noreply.github.com>
2024-07-27 10:21:44 -0600

    Implementing TrainDisplay

--- FinalProject_Announcer/src/ca/ensf380/ucalgary/TestMain.java
+++ FinalProject_Announcer/src/ca/ensf380/ucalgary/TestMain.java
@@ -3,46 +3,13 @@ package ca.ensf380.ucalgary;
  * Referenced code:
  * @author BroCode (Youtube)
  */
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
+
 
 public class TestMain {
 	// Temporary holder for extracting map.csv
-	public static void main(String[] args) {
-		BufferedReader reader = null;
-		String line = "";
-		ArrayList<String[]> Stations = new ArrayList<String[]>();
-		try {
-			// fetches Map.csv
-			reader = new BufferedReader(new FileReader("Map.csv"));
-			// While loop adds and edits the csv for usage in String[] collections
-			while ((line = reader.readLine()) != null) {
-				String[] row = line.split(",");
-				if (row.length > 8) {
-					// removes quotes
-					row[7] = row[7].replaceAll("\"", "");
-					row[8] = row[8].replaceAll("\"", "");
-				}
-				// Removes leading whitespaces
-				row[4] = row[4].trim();
-				Stations.add(row);
-			}
-		}
-		catch(Exception e) {
-			e.printStackTrace();
-		}
-		finally {
-			try {
-				reader.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		TrainDisplay Display = new TrainDisplay();
-		Display.setStations(Stations);
-		Display.TrainSim();
+	public static void main(String[] args) { 
+		// Implement GUI section
+		TrainDisplay display = new TrainDisplay();
+		display.activateTrainDisplay();
 	}
-
 }
--- FinalProject_Announcer/src/ca/ensf380/ucalgary/TrainDisplay.java
+++ FinalProject_Announcer/src/ca/ensf380/ucalgary/TrainDisplay.java
@@ -1,66 +1,121 @@
 package ca.ensf380.ucalgary;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
-public final class TrainDisplay {
+import java.io.File;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+public class TrainDisplay {
+	private String currentTrain;
 	private ArrayList<String[]> stations;
-	private final static Announcer announcer = new Announcer();
-	
-	public TrainDisplay(ArrayList<String[]> stations) {
-		this.stations = stations;
-	}
+	private Announcer announcer;
 	public TrainDisplay() {
+		BufferedReader reader = null;
+		String line;
+		this.stations = new ArrayList<String[]>();
+		this.announcer = new Announcer();
+		try {
+			// fetches Map.csv
+			reader = new BufferedReader(new FileReader("Map.csv"));
+			// While loop adds and edits the csv for usage in String[] collections
+			while ((line = reader.readLine()) != null) {
+				String[] row = line.split(",");
+				if (row.length > 8) {
+					// removes quotes
+					row[7] = row[7].replaceAll("\"", "");
+					row[8] = row[8].replaceAll("\"", "");
+				}
+				// Removes leading whitespace
+				row[4] = row[4].trim();
+				stations.add(row);
+			}
 
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			}
+		finally {
+			try {
+				reader.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
+
 	public ArrayList<String[]> getStations() {
 		return this.stations;
 	}
 	public void setStations(ArrayList<String[]> stations) {
 		this.stations = stations;
 	}
-	public void playAnnouncer() {
-		announcer.playAnnouncer();
+	public String getCurrentTrain() {
+		return this.currentTrain;
 	}
-	public void TrainSim() {
-		// Starts on blue line
-		// Then red line
-		// Finally green line
-		boolean redLine = false;
-		boolean blueLine = false;
-		boolean greenLine = false;
-		// 
-		int currentPos = 1;
-		final int redEndRow = 43;
-		final int blueEndRow = 87;
-		final int greenEndRow = 120;
+	public void setCurrentTrain(String currentTrain) {
+		this.currentTrain = currentTrain;
+	}
+
+	public Announcer getAnnouncer() {
+		return this.announcer;
+	}
+	public void setAnnouncer(Announcer announcer) {
+		this.announcer = announcer;
+	}
+	public void activateTrainDisplay() {
+		// 1920x1080 resolution
+		EventQueue.invokeLater(() -> {
+			String file = "Stations.png";
+			JPanel stations = new JPanel();
+			BufferedImage myPicture = null;
+			String[] nextStations = new String[4];
+			try {
+				myPicture = ImageIO.read(new File(file));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			JLabel station1 = new JLabel("Maplewood Station");
+			JLabel station2 = new JLabel("Maplewood Station");
+			JLabel station3 = new JLabel("Maplewood Station");
+			JLabel station4 = new JLabel("Maplewood Station");
+			JLabel station5 = new JLabel("Maplewood Station");
+			JLabel imgStations = new JLabel(new ImageIcon(myPicture));
+			stations.add(imgStations);
+			stations.setBounds(0,0,1920,270);
+			station1.setBounds(290,0,1920,270);
+			station2.setBounds(595,0,1920,270);
+			station3.setBounds(905,0,1920,270);
+			station4.setBounds(1215,0,1920,270);
+			station5.setBounds(1525,0,1920,270);
+
+			JFrame frame = new JFrame("Test Frame");
+			frame.setLayout(null);
+			frame.setSize(1920, 1080);
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.setVisible(true);
+			frame.add(station1);
+			frame.add(station2);
+			frame.add(station3);
+			frame.add(station4);
+			frame.add(station5);
+			frame.add(stations);
+			frame.setResizable(false);
+			updateNext();
+			
+		});
+
 		
-		while (true) {
-			// Resets train lines
-			if (redLine && blueLine && greenLine) {
-				redLine = false;
-				blueLine = false;
-				greenLine = false;
-				break; // Use continue to keep the loop
-			}
-			// If red line ends, change to blue line start
-			if (!redLine) {
-				if (currentPos == blueEndRow)
-					redLine = true;
-			}
-			// If blue line ends, change to green line start
-			else if (!blueLine) {
-				if (currentPos == redEndRow)
-					blueLine = true;
-			}
-			// If green line ends, all lines reset to false
-			else {
-				if (currentPos == greenEndRow)
-					greenLine = true;
-			}
-			// Plays "Next stop: ______ station"
-			announcer.setstation(stations.get(currentPos)[4]);
-			playAnnouncer();
-			currentPos++;
-		}
 	}
+
+	private void updateNext() {
+		// TODO Auto-generated method stub
+		
+	}
+
 
 }
