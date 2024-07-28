678a4f6dde7a36528aac3afa49ea702bf8696399
Saif Youssef <seifeldiny1@gmail.com>
2024-07-27 20:43:43 -0600

    testZ

--- weatherReport/src/subwayScreen/newsApi.java
+++ weatherReport/src/subwayScreen/newsApi.java
@@ -1,10 +1,24 @@
 package subwayScreen;
 
-public class newsApi {
+import java.net.URL;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class newsApi{
+	public static void newsApiFetch(String[] args ) {
+		try {
+			String urli = "https://www.thenewsapi.com/";
+			String apiKey = "fc7xQuAZMDSj0rMBFUEHh5a31KpbJHIxT6hZ4ZLL";
+			String authUrl = urli + apiKey;
+			URI uri = new URI(authUrl);
+			URL url = uri.toURL();
+			
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("GET");
+			connection.connect();
+			
+		} 
 	}
-
 }
