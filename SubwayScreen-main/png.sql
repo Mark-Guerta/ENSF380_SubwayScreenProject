511664a729ee84b2072e73d279ed219ec0bdc636
Mark Guerta <144377385+Mark-Guerta@users.noreply.github.com>
2024-08-04 08:14:29 -0600

    Added png.sql

---
+++ SubwayScreen-main/png.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE IF NOT EXISTS png;
+USE png;
+CREATE TABLE IF NOT EXISTS png_files(
+	file_name varchar(255),
+    file_path varchar(255)
+);
+TRUNCATE TABLE png_files;
+INSERT INTO png_files VALUES ("AD1","C:\\Users\\Mark\\eclipse-workspace\\ENSF380_SubwayScreenProject\\SubwayScreen-main\\ADs\\Screenshot 2024-08-01 at 4.04.32 PM.png");
+INSERT INTO png_files VALUES ("AD2","C:\\Users\\Mark\\eclipse-workspace\\ENSF380_SubwayScreenProject\\SubwayScreen-main\\ADs\\Screenshot 2024-08-01 at 4.04.41 PM.png");
+INSERT INTO png_files VALUES ("AD3","C:\\Users\\Mark\\eclipse-workspace\\ENSF380_SubwayScreenProject\\SubwayScreen-main\\ADs\\Screenshot 2024-08-01 at 4.04.50 PM.png");
+INSERT INTO png_files VALUES ("AD4","C:\\Users\\Mark\\eclipse-workspace\\ENSF380_SubwayScreenProject\\SubwayScreen-main\\ADs\\Screenshot 2024-08-01 at 4.04.56 PM.png");
+INSERT INTO png_files VALUES ("AD5","C:\\Users\\Mark\\eclipse-workspace\\ENSF380_SubwayScreenProject\\SubwayScreen-main\\ADs\\Screenshot 2024-08-01 at 4.05.08 PM.png");
+INSERT INTO png_files VALUES ("AD6","C:\\Users\\Mark\\eclipse-workspace\\ENSF380_SubwayScreenProject\\SubwayScreen-main\\ADs\\Screenshot 2024-08-01 at 4.05.20 PM.png");
+INSERT INTO png_files VALUES ("AD7","C:\\Users\\Mark\\eclipse-workspace\\ENSF380_SubwayScreenProject\\SubwayScreen-main\\ADs\\Screenshot 2024-08-01 at 4.05.57 PM.png");
+
