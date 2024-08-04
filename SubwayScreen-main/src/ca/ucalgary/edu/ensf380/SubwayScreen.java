6e31c47a12f0855147792da472b71bdf15cb7601
Saif Youssef <seifeldiny1@gmail.com>
2024-08-04 03:08:12 -0600

    created javadoc

---
+++ SubwayScreen-main/src/ca/ucalgary/edu/ensf380/SubwayScreen.java
@@ -0,0 +1,252 @@
+/**
+ * Copyright (c) 2022-2023 Mahdi Jaberzadeh Ansari and others.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *	
+ *	The above copyright notice and this permission notice shall be
+ *	included in all copies or substantial portions of the Software.
+ *	
+ *	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *	MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ *	NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ *	LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ *	OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ *	WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package ca.ucalgary.edu.ensf380;
+import java.awt.Color;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.regex.*;
+import javax.swing.JFrame;
+
+/**
+ * SubwayScreen.java
+ * Simulates and displays subway screen.
+ * @author  Mahdi Ansari
+ * @author Mark Guerta
+ * @author Saif Youssef
+ * @version 0.9
+ */
+public class SubwayScreen {
+	/**
+	 * Unused constructor
+	 */
+	public static ArrayList<String[]> stations;
+	private SubwayScreen() {
+		
+	}
+	/**
+	 * Main method
+	 * @param args Command Line Arguments
+	 */
+	public static void main(String[] args) {
+        // Runs the simulator 
+		Process process = null;        
+        try {
+        	String[] command = {"java", "-jar", "./exe/SubwaySimulator.jar", "--in", "./data/subway.csv", "--out", "./out"};
+        	process = new ProcessBuilder(command).start();
+        } catch (IOException e) {
+        	System.err.println(e);
+            e.printStackTrace();
+        }
+        final Process finalProcess = process;
+
+        // It will destroy the simulator process at the end 
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            if (finalProcess != null) {
+                finalProcess.destroy();
+            }
+        }));
+        
+        // Keep the application alive for 5 minutes 
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                if (finalProcess != null) {
+                    finalProcess.destroy();
+                }
+                timer.cancel();
+            }
+        }, 5 * 60 * 1000); // 5 minutes in milliseconds
+
+        // Prints simulator in the console. 
+        // Just for test. Its while loop friezes the application.  
+        InputStream inputStream = process.getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        String line;
+        String train = null;
+        Pattern pattern = null;
+        try {
+        	// Checks if args[0] contains a train number
+            if (args[0].matches("T[1-9]") || args[0].matches("T1[0-2]"))
+        		 pattern = Pattern.compile("("+ args[0] +"\\([RGB]\\d\\d, [FB]\\))");
+            else
+            	throw new IllegalArgumentException("Invalid argument for train");
+        }
+        catch(IllegalArgumentException IA) {
+        	IA.printStackTrace();
+        	System.exit(1);
+        }
+        catch (Exception e) {
+        	e.printStackTrace();
+        	System.exit(1);
+        } 
+        String[] trainLine = new String[4];
+        String [] trains;
+        TrainDisplay trainDisplay = null;
+        WeatherDisplay weatherDisplay = null;
+        AdvertisementDisplay advertisementDisplay = null;
+		NewsDisplay newsDisplay = null;
+		stations = readMapCSV("map.csv");
+		// Creates new window
+        JFrame frame = new JFrame();
+        try {
+        	while (true) {
+        		line = reader.readLine();
+        		if (line != null) {
+        			System.out.println(line);
+        			for (int i = 0;i < 4; i++)
+        				if (trainLine[i] == null) {
+        					trainLine[i] = line;
+        					break;
+        				}
+        		}
+        		if (trainLine[3] == null)
+        			continue;
+        		trains = extractTrains(trainLine);
+    			// Formats and adds displays to the frame
+    			if (trainDisplay == null) {
+    				for (int i = 0; i < 12; i++) {
+        				Matcher matcher = pattern.matcher(trains[i]);
+            			if(matcher.find()) {
+            				train = matcher.group(1);
+            			}
+    				}
+    				trainDisplay = new TrainDisplay(train);
+   					weatherDisplay = new WeatherDisplay();
+   					newsDisplay = new NewsDisplay();
+   					advertisementDisplay = new AdvertisementDisplay(trains,train);
+   					
+    				if (trainDisplay == null || newsDisplay == null || weatherDisplay == null || advertisementDisplay == null)
+    					throw new Exception("Failed display construction");
+    				
+    				frame.add(trainDisplay.getDisplay());
+   					frame.add(newsDisplay.getDisplay());
+    				frame.add(weatherDisplay.getDisplay());
+    				frame.add(advertisementDisplay.getDisplay());
+    				frame.setLayout(null);
+    				frame.setSize(1095, 720);
+    				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    				frame.setVisible(true);
+   					frame.setResizable(false);
+   					if (trainDisplay.getDirection().equals("F"))
+   						Announcer.playAnnouncer(stations.get(trainDisplay.getCurrentStation() + 1)[4]);
+   					else
+   						Announcer.playAnnouncer(stations.get(trainDisplay.getCurrentStation() - 1)[4]);
+   	    			Arrays.fill(trainLine,null);
+   	    			Arrays.fill(trains,null);
+   	    			continue;
+    				}
+    			// Updates display contents
+    			if (trains[11] != null) {
+        			newsDisplay.updateDisplay();
+        			weatherDisplay.updateDisplay();
+        			advertisementDisplay.setTrainList(trains);
+        			advertisementDisplay.updateDisplay();
+        			if (train.contains("F"))
+        				trainDisplay.setDirection("F");
+    				else
+    					trainDisplay.setDirection("B");
+        			trainDisplay.updateDisplay();
+    	    		Arrays.fill(trainLine,null);
+    	    		Arrays.fill(trains,null);
+    			}
+        	}
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        catch (Exception e) {
+        	e.printStackTrace();
+        	System.exit(1);
+        }
+    }
+	
+	/**
+	 * Extracts station data from map.csv
+	 * @param fileName File path for csv file
+	 * @return stations	Collection of station information
+	 */
+	public static ArrayList<String[]> readMapCSV(String fileName) {
+		BufferedReader reader = null;
+		ArrayList<String[]> stations = new ArrayList<String[]>();;
+		String line;
+
+		try {
+			// fetches csv.file
+			reader = new BufferedReader(new FileReader(fileName));
+			// While loop adds and edits the CSV for usage in String[] arrayList
+			while ((line = reader.readLine()) != null) {
+				String[] row = line.split(",");
+				if (row.length > 8) {
+					// removes quotes
+					row[7] = row[7].replaceAll("\"", "");
+					row[8] = row[8].replaceAll("\"", "");
+				}
+				// Removes leading whitespace
+				row[4] = row[4].trim();
+				stations.add(row);
+			}
+			return stations;
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			return null;
+			}
+		finally {
+			try {
+				reader.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	public static String[] extractTrains(String[] trainLine) {
+		Pattern trainPattern = Pattern.compile("T\\d+?\\([RGB]\\d\\d, [FB]\\)");
+
+		String[] trains = new String[12];
+		for (int i = 1; i < 4; i++) {
+			Matcher matcher = trainPattern.matcher(trainLine[i]);
+			for (int j = 0; j < 4; j++) {
+				matcher.find();
+				if (i == 1) {
+					trains[j] = matcher.group();
+				}
+				else if (i == 2) {
+					trains[j + 4] = matcher.group();
+				}
+				else if (i == 3) {
+					trains[j + 8] = matcher.group();
+				}
+			}
+		}
+
+		return trains;
+	}
+}
--- SubwayScreen-main/src/ca/ucalgary/edu/ensf380/TrainDisplay.java
+++ SubwayScreen-main/src/ca/ucalgary/edu/ensf380/TrainDisplay.java
@@ -7,54 +7,141 @@ import java.io.File;
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 import javax.swing.*;
-public class TrainDisplay {
+public class TrainDisplay extends Display {
 	private int currentStation;
 	private String Line;
 	private String direction;
 	private ArrayList<String[]> stationArray;
 	private JLabel[] stations;
-	private final int redFirst = 1;
-	private final int blueFirst = 45;
-	private final int greenFirst = 89;
-	private final int redLast = 44;
-	private final int blueLast = 88;
-	private final int greenLast = 120;
-	public TrainDisplay(String station, ArrayList<String[]> stationArray) {
-		
-		Pattern pattern = Pattern.compile("(\\d\\d)");
-		Matcher matcher =  pattern.matcher(station);
-		this.stationArray = stationArray;
-		matcher.find();
-		this.currentStation = Integer.parseInt(matcher.group(1));
+	// Variables below stay local to the object 
+	private int redFirst;
+	private int blueFirst;
+	private int greenFirst;
+	private int redLast;
+	private int blueLast;
+	private int greenLast;
+	
+	public TrainDisplay(String train) {
+		// Constructor initializes values based on map.csv
+		super();
 		this.stations = new JLabel[5];
-		for (int i = 0; i < 5; i++) {
-			stations[i] = new JLabel(stationArray.get(currentStation + i)[4]);
+		this.stationArray = SubwayScreen.stations;
+		redFirst = -1;
+		redLast = -1;
+		blueFirst = -1;
+		blueLast = -1;
+		greenFirst = -1;
+		greenLast = -1;
+		// Extracts station from train
+		stationMatcher(train);
+		try {
+			// Sets train line colour
+			if (train.contains("R"))
+				Line = "R";
+			else if (train.contains("G"))
+				Line = "G";
+			else if (train.contains("B"))
+				Line = "B";
+			else
+				throw new Exception("Unknown train line");
+			// Sets train direction
+			if (train.contains("F"))
+				direction = "F";
+			else if (train.contains("B"))
+				direction = "B";
+			else
+				throw new Exception("Train direction error");
+			// Sets train line start and end
+			for (int i = 1;i < stationArray.size();i++) {
+				if (stationArray.get(i)[1].contains("R") && redFirst == -1) {
+					redFirst = Integer.parseInt(stationArray.get(i)[0]);		
+				}
+				else if(stationArray.get(i)[1].contains("B") && redLast == -1) {
+					redFirst = Integer.parseInt(stationArray.get(i - 1)[0]);
+					blueFirst = Integer.parseInt(stationArray.get(i)[0]);
+				}
+				else if(stationArray.get(i)[1].contains("G") && blueLast == -1 && stationArray.getLast()[1].contains("G")) {
+					blueLast = Integer.parseInt(stationArray.get(i - 1)[0]);
+					greenFirst = Integer.parseInt(stationArray.get(i)[0]);
+					break;
+				}
+			}
 		}
-		stations[0].setBounds(25, 0, 1080, 180);
-		stations[1].setBounds(260, 0, 1080, 180);
-		stations[2].setBounds(500, 0, 1080, 180);
-		stations[3].setBounds(720, 0, 1080, 180);
-		stations[4].setBounds(930, 0, 1080, 180);
+		catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+		// Creates JLabel Stations to show on screen
+		boolean check = false;
+		stations[0] = new JLabel(stationArray.get(currentStation)[4]);
+		for (int j = 1; j < 5; j++) {
+			stations[j] = new JLabel("",SwingConstants.CENTER);
+			if (direction.equals("F")) {
+				if (currentStation == redLast || currentStation == blueLast || currentStation == greenLast) {
+					check = true;
+					stations[j].setText("");
+				}
+				else if(check) {
+					stations[j].setText("");
+				}
+				else {
+					stations[j].setText(stationArray.get(currentStation + j)[4]);
+				}	
+			}
+			else {
+				if (currentStation == redLast || currentStation == blueLast || currentStation == greenLast) {
+					check = true;
+					stations[j].setText("");
+				}
+				else if(check) {
+					stations[j].setText("");
+				}
+				else {
+					stations[j].setText(stationArray.get(currentStation - j)[4]);
+				}	
+			}
+		}
+		// Adjusts JLabel position
+		stations[0].setBounds(15, 0, 200, 180);
+		stations[1].setBounds(210, 0, 200, 180);
+		stations[2].setBounds(435, 0, 200, 180);
+		stations[3].setBounds(670, 0, 200, 180);
+		stations[4].setBounds(900, 0, 200, 180);
+		
+		String file = "stations720p.png";
+		BufferedImage myPicture = null;
+
+		try {
+			myPicture = ImageIO.read(new File(file));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		// Adds images and JLabels to JLayeredPane then to frame
+		JLabel imgStations = new JLabel(new ImageIcon(myPicture));
+		imgStations.setSize(imgStations.getPreferredSize());
+		display.add(imgStations, JLayeredPane.DEFAULT_LAYER);
+		display.setBounds(0, 0, 1080, 120);
+		for (int i = 0; i < 5; i++)
+			display.add(stations[i], JLayeredPane.PALETTE_LAYER);
+
 	}
 
-
-	public int getCurrentTrain() {
+	public int getCurrentStation() {
 		return currentStation;
 	}
 
-
-	public void setCurrentTrain(int currentTrain) {
-		this.currentStation = currentTrain;
+	public void setCurrentStation(int currentStation) {
+		this.currentStation = currentStation;
 	}
 
-	public JLabel[] getStation() {
-		return stations;
+	public String getLine() {
+		return Line;
 	}
 
-
-	public void setStation(JLabel[] station) {
-		this.stations = station;
+	public void setLine(String line) {
+		Line = line;
 	}
+
 	public String getDirection() {
 		return direction;
 	}
@@ -62,78 +149,87 @@ public class TrainDisplay {
 	public void setDirection(String direction) {
 		this.direction = direction;
 	}
-	public String getLine() {
-		return Line;
-	}
-	public void setLine(String line) {
-		Line = line;
-	}
-	public void activateTrainDisplay(JFrame frame) {
-		// 1080x180 resolution
 
-		String file = "stations720p.png";
-		JPanel nextStations = new JPanel();
-		BufferedImage myPicture = null;
-		try {
-			myPicture = ImageIO.read(new File(file));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		JLabel imgStations = new JLabel(new ImageIcon(myPicture));
-		nextStations.add(imgStations);
-		nextStations.setBounds(0, 0, 1080, 180);
-		for (int i = 0; i < 5; i++)
-			frame.add(stations[i]);
-		frame.add(nextStations);
+	public ArrayList<String[]> getStationArray() {
+		return stationArray;
 	}
-	public void updateForward(String station) {
-		Pattern pattern = Pattern.compile("(\\d\\d)");
-		Matcher matcher =  pattern.matcher(station);
-		matcher.find();
-		this.currentStation = Integer.parseInt(matcher.group(1));
-		boolean check = false;
-		for (int i = 0; i < 5; i++) {
-			if (currentStation == redLast || currentStation == blueLast || currentStation == greenLast) {
-				check = true;
-				stations[i].setText("");
-				stations[i].revalidate();
-			}
-			else if(check) {
-				stations[i].setText("");
-				stations[i].revalidate();
-			}
-			else {
-				stations[i].setText(stationArray.get(currentStation + i)[4]);
-				stations[i].revalidate();
-			}	
-		}
-		Announcer.playAnnouncer(stationArray.get(currentStation + 1)[4]);
+
+	public void setStationArray(ArrayList<String[]> stationArray) {
+		this.stationArray = stationArray;
 	}
-	public void updateBackward(String station) {
-		Pattern pattern = Pattern.compile("(\\d\\d)");
-		Matcher matcher =  pattern.matcher(station);
-		matcher.find();
-		this.currentStation = Integer.parseInt(matcher.group(1));
+
+	public JLabel[] getStations() {
+		return stations;
+	}
+
+	public void setStations(JLabel[] stations) {
+		this.stations = stations;
+	}
+
+	public JLayeredPane getDisplay() {
+		return display;
+	}
+
+	public void setDisplay(JLayeredPane display) {
+		this.display = display;
+	}
+	// Updates train stations on screen
+	public void updateDisplay() {
 		boolean check = false;
-		for (int i = 0; i < 5; i++) {
-			if (currentStation == redFirst || currentStation == blueFirst || currentStation == greenFirst) {
-				check = true;
-				stations[i].setText("");
-				stations[i].revalidate();
-			}
-			else if(check) {
-				stations[i].setText("");
-				stations[i].revalidate();
-			}
+		if(direction.equals("F")) {
+			currentStation = currentStation + 1;
+			for (int i = 0; i < 5; i++) {
+				// Checks if at the end of line
 				
-			else {
-				stations[i].setText(stationArray.get(currentStation - i)[4]);
-				stations[i].revalidate();
+				if (currentStation == redLast || currentStation == blueLast || currentStation == greenLast) {
+					check = true;
+					stations[i].setText("");
+					stations[i].revalidate();
+				}
+				else if(check) {
+					stations[i].setText("");
+					stations[i].revalidate();
+				}
+				else {
+					stations[i].setText(stationArray.get(currentStation + i)[4]);
+					stations[i].revalidate();
+				}	
+			}
+			Announcer.playAnnouncer(stationArray.get(currentStation + 1)[4]);
+		}
+		else{
+			currentStation = currentStation - 1;
+			for (int j = 0; j < 5; j++) {	
+				if (currentStation == redFirst || currentStation == blueFirst || currentStation == greenFirst) {
+					check = true;
+					stations[j].setText("");
+					stations[j].revalidate();
+				}
+				else if(check) {
+					stations[j].setText("");
+					stations[j].revalidate(); 
+				}	
+				else {
+					stations[j].setText(stationArray.get(currentStation - j)[4]);
+					stations[j].revalidate();
+				}
+			}
+			Announcer.playAnnouncer(stationArray.get(currentStation - 1)[4]);
+		}
+
+	}
+	// Sorts extracting station number from train
+	private void stationMatcher(String train) {
+		Pattern pattern = Pattern.compile("([RGB]\\d\\d)");
+		Matcher matcher =  pattern.matcher(train);
+		matcher.find();
+		String stationCode = matcher.group(1);
+		for (int i = 0; i < stationArray.size(); i++) {
+			if (stationCode.equals(stationArray.get(i)[3])) {
+				currentStation = Integer.parseInt(stationArray.get(i)[0]);
+				break;
 			}
 		}
-		Announcer.playAnnouncer(stationArray.get(currentStation + 1)[4]);
+
 	}
-
-
-
 }
