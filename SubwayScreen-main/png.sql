072533d533e4ca05257b164961e6df3ab3c5cc60
Arion <144377385+Mark-Guerta@users.noreply.github.com>
2024-08-05 10:15:51 -0600

    Update png.sql

--- SubwayScreen-main/png.sql
+++ SubwayScreen-main/png.sql
@@ -1,8 +1,8 @@
 CREATE DATABASE IF NOT EXISTS png;
 USE png;
 CREATE TABLE IF NOT EXISTS png_files(
-	fileName varchar(255),
-    filePath varchar(255)
+	filename varchar(255),
+    filepath varchar(255)
 );
 TRUNCATE TABLE png_files;
 INSERT INTO png_files VALUES ("AD1","C:\\Users\\Mark\\eclipse-workspace\\ENSF380_SubwayScreenProject\\SubwayScreen-main\\ADs\\Screenshot 2024-08-01 at 4.04.32 PM.png");
